11bdc8e2060210128c707b00ff596ac97cb9ae7f
ik013043z1 <ik013043z1@hz341921-206-21.H013043.NET>
2019-01-07 10:26:18 +0100

    Creamos programa password

---
+++ POO/src/Password/Password.java
@@ -0,0 +1,32 @@
+package Password;
+
+public class Password {
+
+	private int longitud;
+	private String contraseña;
+	
+	public int getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+	public boolean esFuerte() {
+		
+	}
+	
+	public String generarPassword() {
+		
+	}
+	
+	public int longitudPassword(Stribg pass) {
+		
+	}
+}
---
+++ POO/src/Password/ejecutable.java
@@ -0,0 +1,11 @@
+package Password;
+
+public class ejecutable {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
