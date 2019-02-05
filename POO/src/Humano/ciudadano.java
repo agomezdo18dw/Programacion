af4908f8a6a14035c2f89171970967c664ac1145
ik013043z1 <ik013043z1@hz341921-206-21.H013043.NET>
2019-02-05 13:51:20 +0100

    humano herencia

---
+++ POO/src/Humano/ciudadano.java
@@ -0,0 +1,17 @@
+package Humano;
+
+class Ciudadano extends Humano {
+	protected String documento;
+	
+	public Ciudadano(String nombre,String apellido, String documento) {
+        // hace referencia a la clase Humano
+		super(nombre,apellido);
+        this.documento = documento;
+    }
+ 
+	public String identificacion() {
+        // super.identificacion hace referencia a la clase Humano
+        return super.identificacion() + ", documento: " + this.documento;
+    }
+}
+
